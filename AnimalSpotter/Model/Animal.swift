308908e9e2d96ef87483ad839becefcd678c8b78
Christopher Aronson <christopheraronson@icloud.com>
2019-05-09 09:19:03 -0700

    Created User, Bearer, and Animal Models

---
+++ AnimalSpotter/Model/Animal.swift
@@ -0,0 +1,19 @@
+//
+//  Animal.swift
+//  AnimalSpotter
+//
+//  Created by Christopher Aronson on 5/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Animal: Codable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let timeSeen: Date
+    let description: String
+    let imageURL: String
+}
---
+++ AnimalSpotter/Model/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Christopher Aronson on 5/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
