040328807cab720266fb17bbde1e7706e3525f71
Christopher Aronson <christopheraronson@icloud.com>
2019-05-09 10:11:34 -0700

    Built Sign in and Sign up methods

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -17,12 +17,95 @@ enum HTTPMethod: String {
 class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
-    
+    var bearer: Bearer?
     // create function for sign up
+    func signUp(with user: User, completion: @escaping (Error?) -> Void) {
+        let signUpURL = baseUrl.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { ( _, response, error) in
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+    }
+    
     
     // create function for sign in
+    func signIn(with user: User, completion: @escaping (Error?) -> Void) {
+        let signInURL = baseUrl.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: signInURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { ( data, response, error) in
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            }.resume()
+    }
     
     // create function for fetching all animal names
+    
+    // create function for fetching specific animal names
     
     // create function to fetch image
 }
